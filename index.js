9c3922bed0974026021c1437e9ec3e9d389881b1
Frank Bi <frankbi322@gmail.com>
2017-07-30 02:47:06 -0700

    finish google oauth

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
     }
   )
 );
