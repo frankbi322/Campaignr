c602bb9d734bec4b0d9b1e52ad0abaa6657aaf16
Frank Bi <frankbi322@gmail.com>
2017-08-01 15:57:02 -0700

    add default route handler

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 
 module.exports = app => {
+  app.get('/', (req, res) => {
+    res.send({ bye: 'buddy' });
+  });
+
   app.get(
     '/auth/google',
     passport.authenticate('google', {
