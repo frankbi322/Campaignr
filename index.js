35fb731fe9e3c29fa9c8b3cb07bfe59cfd2979a5
Frank Bi <frankbi322@gmail.com>
2017-08-03 02:46:00 -0700

    trying fix

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log('server successfully started on port ' + PORT);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
 });
