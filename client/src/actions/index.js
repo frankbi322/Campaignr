805c34271bf20118e67bfdf3639cab0247e5eef4
Frank Bi <frankbi322@gmail.com>
2017-08-21 02:42:32 -0700

    finish frontend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -37,9 +37,7 @@ export const submitSurvey = (values, history) => async dispatch => {
   });
 };
 
-export const fetchSurveys = () => {
-  async dispatch => {
-    const res = await axios.get('/api/surveys');
-    dispatch({ type: FETCH_SURVEYS, payload: res.data });
-  };
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
